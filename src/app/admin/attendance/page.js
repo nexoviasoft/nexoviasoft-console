563687c905e544367cc4f7fb8988891142ebd899
Aftab Farhan <aftabfarhan324@gmail.com>
2026-01-25 00:20:59 +0600

    Attendance  responsived done

--- src/app/admin/attendance/page.js
+++ src/app/admin/attendance/page.js
@@ -1,5 +1,4 @@
 "use client";
-
 import React from "react";
 import AttendanceHeader from "@/components/admin/attendance/AttendanceHeader";
 import AttendanceStats from "@/components/admin/attendance/AttendanceStats";
@@ -8,7 +7,7 @@ import BiometricClockIn from "@/components/admin/attendance/BiometricClockIn";
 
 export default function Attendance() {
   return (
-    <div className="px-8 py-8 text-white">
+    <div className="px-4 py-6 md:px-8 md:py-8 text-white">
       <div className="max-w-[1600px] w-full mx-auto space-y-6">
         <AttendanceHeader />
         <BiometricClockIn />
