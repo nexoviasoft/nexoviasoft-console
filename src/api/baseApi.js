0ddac734ffd575f3d246bfaf114afa8d2dd0875c
macbookair <macbookair@macbookairs-MacBook-Air.local>
2026-01-27 18:42:44 +0600

    update

--- src/api/baseApi.js
+++ src/api/baseApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 // Base API configuration
 const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.NEXT_PUBLIC_API_URL || 'https://squadlog-backend.up.railway.app',
+  baseUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001',
   // ttps://squadlog-backend.up.railway.app
   prepareHeaders: (headers, { getState }) => {
     // Get token from localStorage
